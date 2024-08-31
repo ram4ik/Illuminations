389b8ae7d6327d6fba04af232a3213631fb83f70
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-08-31 10:56:17 -0700

    init

--- Illuminations/ContentView.swift
+++ Illuminations/ContentView.swift
@@ -7,15 +7,40 @@
 
 import SwiftUI
 
-struct ContentView: View {
+@Observable
+class Illumination {
+    var isOn: Bool = false
+}
+
+struct MyView: View {
+    @Bindable var lightBackground: Illumination
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Toggle(isOn: $lightBackground.isOn) {
+                Text("Lights")
+            }.fixedSize()
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    @State private var light = Illumination()
+    var body: some View {
+        VStack {
+            MyView(lightBackground: light)
+            if light.isOn {
+                Text("On")
+            } else {
+                Text("Off")
+                    .colorInvert()
+            }
+            Divider()
+            Text("Example of bindable var")
+                .foregroundStyle(light.isOn ? .primary : Color.white)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(light.isOn ? Color.white : Color.blue)
     }
 }
 
