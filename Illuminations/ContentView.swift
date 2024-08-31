04269a4cbbc0b4bb5397f55907fc5288e8ae63a5
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-08-31 10:48:19 -0700

    Initial Commit

---
+++ Illuminations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Illuminations
+//
+//  Created by test on 8/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Illuminations/IlluminationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IlluminationsApp.swift
+//  Illuminations
+//
+//  Created by test on 8/31/24.
+//
+
+import SwiftUI
+
+@main
+struct IlluminationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
